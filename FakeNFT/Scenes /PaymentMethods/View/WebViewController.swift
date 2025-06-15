79f9a9888c2a4a8d098ee85140bb52cc4931fe28
IceRedline <scpd2604@gmail.com>
2025-06-15 15:06:35 +0300

    feat(WebViewController): Добавлен предупреждение про WebView

--- FakeNFT/Scenes /PaymentMethods/View/WebViewController.swift
+++ FakeNFT/Scenes /PaymentMethods/View/WebViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 import WebKit
 
+
+
 final class WebViewController: UIViewController {
+    
+#warning("Ни у кого из партнёров по команде, и у меня на разных устройствах в том числе не получилось вызвать краш при откртыии WebView.")
     
     var webView = WKWebView()
     var loadingProgressBar = UIProgressView()
